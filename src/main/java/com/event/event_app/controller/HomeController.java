267ff00a6967254bb76ee73d8ef11f1b25c69195
klausendias <klaus_dias1@hotmail.co.uk>
2019-06-27 11:27:15 +0100

    added method for homepage

--- src/main/java/com/event/event_app/controller/HomeController.java
+++ src/main/java/com/event/event_app/controller/HomeController.java
@@ -1,8 +1,13 @@
 package com.event.event_app.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HomeController {
-
+	
+	@RequestMapping("/")
+public String home() {
+		return "Hello everyone";
+	}
 }
