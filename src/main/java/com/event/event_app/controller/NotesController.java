7e0a53c2aae28cc3e91eb888cb73e7b6ae5bc630
klausendias <klaus_dias1@hotmail.co.uk>
2019-06-27 14:02:21 +0100

    added new version dependencies

--- src/main/java/com/event/event_app/controller/NotesController.java
+++ src/main/java/com/event/event_app/controller/NotesController.java
@@ -1,16 +1,26 @@
 package com.event.event_app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.event.event_app.model.Note;
+import com.event.event_app.repository.NotesRepository;
 
 @RestController
 @RequestMapping("api/v1")
 public class NotesController {
+	
+	@Autowired
+	private NotesRepository notesRepository;
 
 	//value is URL local host . api / v1 . notes
 	@RequestMapping(value ="notes", method = RequestMethod.GET)
 	public List<Note> list(){
-		
+		return notesRepository.findAll();
 	}
 	
 }
--- src/main/java/com/event/event_app/repository/NotesRepository.java
+++ src/main/java/com/event/event_app/repository/NotesRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.event.event_app.model.Note;
 
-public interface NotesRepository extends JpaRepository<Note, Long> {
+public interface NotesRepository 
+		extends JpaRepository<Note, Long> {
 
 }
