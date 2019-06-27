d3b4d4c616b73fb2330213d189c5424bef517130
klausendias <klaus_dias1@hotmail.co.uk>
2019-06-27 10:51:35 +0100

    initial project structure and gitignore file

---
+++ src/main/java/com/event/event_app/App.java
@@ -0,0 +1,13 @@
+package com.event.event_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
