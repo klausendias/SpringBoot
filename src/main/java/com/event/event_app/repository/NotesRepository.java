8e7a0706b84190e7186954e18b8f654344189e9f
klausendias <klaus_dias1@hotmail.co.uk>
2019-06-27 12:35:10 +0100

    added jpa Repository

---
+++ src/main/java/com/event/event_app/repository/NotesRepository.java
@@ -0,0 +1,9 @@
+package com.event.event_app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.event.event_app.model.Note;
+
+public interface NotesRepository extends JpaRepository<Note, Long> {
+
+}
